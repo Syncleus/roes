223e75e9b9c6dad02a623cc5359f07f1a994b3bc
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2017-07-05 21:34:42 -0400

    Added reading of hardware ADC to determine power along with some basic calibration.

--- core/core.ino
+++ core/core.ino
@@ -22,6 +22,14 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define SCREEN_ROW_4_Y 49
 #define SCREEN_ROW_5_Y 57
 
+#define POWER_FWD_PIN A15
+#define POWER_RVR_PIN A14
+
+#define CALIBRATE_FWD_5W 73
+#define CALIBRATE_FWD_200W 689
+#define CALIBRATE_RVR_5W 102
+#define CALIBRATE_RVR_200W 720
+
 static const unsigned char PROGMEM gamma16_glcd_bmp[] =
 { B01111111, B11111110,
   B00111111, B11111110,
@@ -50,73 +58,140 @@ static const unsigned char PROGMEM angle8_glcd_bmp[] =
   B01111110,
   B00000000 };
 
-float power_fwd_test = 100.0;
-boolean power_fwd_increasing = true;
-float power_rvr_test = 100.0;
-boolean power_rvr_increasing = true;
+boolean demo_active = false;
+boolean demo_power_fwd_increasing = true;
+boolean demo_power_rvr_increasing = true;
+
+float power_fwd = 0.0;
+float power_rvr = 0.0;
+float calibrate_fwd_slope = 0.0;
+float calibrate_fwd_intercept = 0.0;
+float calibrate_rvr_slope = 0.0;
+float calibrate_rvr_intercept = 0.0;
 
 void setup()   {                
   Serial.begin(9600);
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+
+  calibrateFwd(5, CALIBRATE_FWD_5W, 200, CALIBRATE_FWD_200W);
+  calibrateRvr(5, CALIBRATE_RVR_5W, 200, CALIBRATE_RVR_200W);
   // init done
 }
 
 void loop() {
-  render(power_fwd_test, power_rvr_test);
+  if( !demo_active )
+    updateInputs();
+  render(power_fwd, power_rvr);
   delay(25);
-  adjustTestValues();
+  if(demo_active)
+    adjustTestValues();
+}
+
+float calculateCalibrationSlope(float lowVoltage, uint16_t lowAdc, float highVoltage, uint16_t highAdc) {
+  return (highVoltage - lowVoltage) / ((float) (highAdc - lowAdc));
+}
+
+float calculateCalibrationIntercept(float slope, float voltage, uint16_t adc) {
+  return voltage - slope * ((float)adc);
+}
+
+void calibrateFwd(float lowPower, uint16_t lowAdc, float highPower, uint16_t highAdc) {
+  float lowVoltage = powerToVoltage(lowPower);
+  float highVoltage = powerToVoltage(highPower);
+  calibrate_fwd_slope = calculateCalibrationSlope(lowVoltage, lowAdc, highVoltage, highAdc);
+  calibrate_fwd_intercept = calculateCalibrationIntercept(calibrate_fwd_slope, highVoltage, highAdc);
+}
+
+void calibrateRvr(float lowPower, uint16_t lowAdc, float highPower, uint16_t highAdc) {
+  float lowVoltage = powerToVoltage(lowPower);
+  float highVoltage = powerToVoltage(highPower);
+  calibrate_rvr_slope = calculateCalibrationSlope(lowVoltage, lowAdc, highVoltage, highAdc);
+  calibrate_rvr_intercept = calculateCalibrationIntercept(calibrate_rvr_slope, highVoltage, highAdc);
+}
+
+float calculateFwdPower(uint16_t adcValue) {
+  float calculatedVoltage = ((float)adcValue) * calibrate_fwd_slope + calibrate_fwd_intercept;
+  return voltageToPower(calculatedVoltage);
+}
+
+float calculateRvrPower(uint16_t adcValue) {
+  float calculatedVoltage = ((float)adcValue) * calibrate_rvr_slope + calibrate_rvr_intercept;
+  return voltageToPower(calculatedVoltage);
+}
+
+float voltageToPower(float voltage) {
+  return (voltage*voltage) / 50.0;
+}
+
+float powerToVoltage(float power) {
+  return sqrt(power * 50.0);
+}
+
+void updateInputs() {
+  updatePowerFwd();
+  updatePowerRvr();
+}
+
+void updatePowerFwd() {
+  uint16_t adcValue = analogRead(POWER_FWD_PIN);
+  power_fwd = calculateFwdPower(adcValue);
+}
+
+void updatePowerRvr() {
+  uint16_t adcValue = analogRead(POWER_RVR_PIN);
+  power_rvr = calculateRvrPower(adcValue);
 }
 
 void adjustTestValues() {
-  if( power_fwd_test < 10.0 )
-    if( power_fwd_increasing )
-      power_fwd_test += 0.1;
+  if( power_fwd < 10.0 )
+    if( demo_power_fwd_increasing )
+      power_fwd += 0.1;
     else
-      power_fwd_test -= 0.1;
-  else if(power_fwd_test >= 100)
-    if( power_fwd_increasing )
-      power_fwd_test += 10.0;
+      power_fwd -= 0.1;
+  else if(power_fwd >= 100)
+    if( demo_power_fwd_increasing )
+      power_fwd += 10.0;
     else
-      power_fwd_test -= 10.0;
+      power_fwd -= 10.0;
   else
-    if( power_fwd_increasing )
-      power_fwd_test += 1.0;
+    if( demo_power_fwd_increasing )
+      power_fwd += 1.0;
     else
-      power_fwd_test -= 1.0;
+      power_fwd -= 1.0;
       
-  if(power_fwd_test > 1000.0) {
-    power_fwd_test = 1000.0;
-    power_fwd_increasing = false;
+  if(power_fwd > 1000.0) {
+    power_fwd = 1000.0;
+    demo_power_fwd_increasing = false;
   }
-  else if(power_fwd_test < 0.0 ) {
-    power_fwd_test = 0.0;
-    power_fwd_increasing = true;
+  else if(power_fwd < 0.0 ) {
+    power_fwd = 0.0;
+    demo_power_fwd_increasing = true;
   }
 
-  if( power_rvr_test < 10.0 )
-    if( power_rvr_increasing )
-      power_rvr_test += 0.2;
+  if( power_rvr < 10.0 )
+    if( demo_power_rvr_increasing )
+      power_rvr += 0.2;
     else
-      power_rvr_test -= 0.2;
-  else if(power_rvr_test >= 100)
-    if( power_rvr_increasing )
-      power_rvr_test += 20.0;
+      power_rvr -= 0.2;
+  else if(power_rvr >= 100)
+    if( demo_power_rvr_increasing )
+      power_rvr += 20.0;
     else
-      power_rvr_test -= 20.0;
+      power_rvr -= 20.0;
   else
-    if( power_rvr_increasing )
-      power_rvr_test += 2.0;
+    if( demo_power_rvr_increasing )
+      power_rvr += 2.0;
     else
-      power_rvr_test -= 2.0;
-  if(power_rvr_test > power_fwd_test) {
-    power_rvr_test = power_fwd_test;
-    power_rvr_increasing = false;
+      power_rvr -= 2.0;
+  if(power_rvr > power_fwd) {
+    power_rvr = power_fwd;
+    demo_power_rvr_increasing = false;
   }
-  else if( power_rvr_test < 0.0 ) {
-    power_rvr_test = 0.0;
-    power_rvr_increasing = true;
+  else if( power_rvr < 0.0 ) {
+    power_rvr = 0.0;
+    demo_power_rvr_increasing = true;
   }
 }
 
@@ -190,7 +265,8 @@ void renderCompleteBar(int8_t y_offset, String label, float value, String units,
 void render(float power_fwd, float power_rvr) {
   display.clearDisplay();
 
-  renderCompleteBar(SCREEN_ROW_1_Y, "SWR", swrFromPower(power_fwd, power_rvr), "", 1.0, 2.0, 2.0);
+  //make sure power_rvr isnt higher than power_fwd
+  renderCompleteBar(SCREEN_ROW_1_Y, "SWR", (power_rvr <= power_fwd ? swrFromPower(power_fwd, power_rvr) : swrFromPower(power_fwd, power_fwd)), "", 1.0, 2.0, 2.0);
   renderCompleteBar(SCREEN_ROW_2_Y, "Fwd", power_fwd, "w", 0.0, 100.0, 2.0);
   renderCompleteBar(SCREEN_ROW_3_Y, "Rvr", power_rvr, "w", 0.0, 100.0, 2.0);
 

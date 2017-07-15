e9c3ed00435f0af7b4fbe41574c23f445728b34b
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2017-07-15 10:34:52 -0400

    Minor formatting cleanup.

--- core/swr_heartbeat.cpp
+++ core/swr_heartbeat.cpp
@@ -10,13 +10,10 @@ void heartbeatUpdate() {
   static unsigned int  ledStatus = LOW;  // Last set LED mode.
   static unsigned long ledBlinkTime = 0; // LED blink time.
 
-  // LED blinking heartbeat. Yes, we are alive.
-  // For explanation, see:
-  // http://playground.arduino.cc/Code/TimingRollover
   if ( (long)(millis() - ledBlinkTime) >= 0 )
   {
     // Toggle LED.
-    ledStatus = (ledStatus==HIGH ? LOW : HIGH);
+    ledStatus = (ledStatus == HIGH ? LOW : HIGH);
 
     // Set LED pin status.
     digitalWrite(HEARTBEAT_LED_PIN, ledStatus);
@@ -24,4 +21,4 @@ void heartbeatUpdate() {
     // Reset "next time to toggle" time.
     ledBlinkTime = millis() + HEARTBEAT_RATE_MS;
   }
-} // End of ledBlink()
+}
